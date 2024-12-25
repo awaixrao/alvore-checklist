573897c953d8ccf00ea7db1c66430385becf8415
Muhammad Awais <Awaixxxxx@gmail.com>
2024-12-25 10:58:48 +0500

    icons work

--- src/UI/PopUp.jsx
+++ src/UI/PopUp.jsx
@@ -1,14 +1,12 @@
+import celebrate from "../assets/celebration.png";
+
 const Popup = ({ onClose }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-50">
       <div className="bg-white rounded-lg shadow-lg p-8 text-center max-w-sm">
         {/* Celebration Icon */}
         <div className="mb-4 flex justify-center">
-          <img
-            src="https://s3-alpha-sig.figma.com/img/6166/65ea/bfe65ee91c188bff2277be6e4ac4a07e?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gMr95414dXWjmcjxXUG5Ga-EAbZKswHGW-IlBht3V3VgHgzQ4iJc0vkj3Db41d9eLFQ6EC9Po0HTYsSr8wQghdBcdOK4dCMgFQPhjwwl4NXcv-zonY-9XGOxPevUMjghnWlCLtmBt8pZgmSfqqM~A1P2C8v-9OuXuv2EhLbv0qhY4V141gSoa61QbGhWzkFOD3nrWY8MI4ydMLyKyEvFHjICgzt7RBDPXjXKfJVbyZIrOj7kZxctLbOL7HvusIgYfKlCM1sUXclV06ac4gbtYXsMiPzTnceC6Jih5B-OejR3Bhz-CA4o7TcwRvACfzvZHJDU11JC1GAn728DRBELFg__" // Replace with your permanent image URL
-            alt="Celebration Icon"
-            className="w-20 h-20"
-          />
+          <img src={celebrate} alt="Celebration Icon" className="w-20 h-20" />
         </div>
 
         {/* Title */}
